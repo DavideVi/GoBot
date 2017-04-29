0b94d2bb69e96e4ea254781f807c8b25b330a0eb
Davide Vitelaru <davide@davidevitelaru.com>
2017-04-29 18:09:29 +0100

    Can now process the request body and store as JSON

--- handlers.go
+++ handlers.go
@@ -1,19 +1,49 @@
 package main
 
 import (
-    "net/http"
-
+  "fmt"
+  "io"
+  "net/http"
+  "io/ioutil"
+  "encoding/json"
 )
-// "encoding/json"
 // "fmt"
 // "github.com/gorilla/mux"
 
+type UserMessage struct {
+  User    string  `json:"user"`
+  Message string  `json:"message"`
+}
 
 func ProcessMessage(w http.ResponseWriter, r *http.Request) {
 
+  // Holds the body of the message in JSON
+  var message UserMessage
 
+  // Reading the body
+  body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+  if err != nil {
+    panic(err)
+  }
+  if err := r.Body.Close(); err != nil {
+    panic(err)
+  }
+  if err := json.Unmarshal(body, &message); err != nil {
+    w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+    w.WriteHeader(422) // unprocessable entity
+    if err := json.NewEncoder(w).Encode(err); err != nil {
+      panic(err)
+    }
+  }
 
-    // fmt.Fprintln(w, "Welcome!")
+  fmt.Println(message)
+
+  // Going through command and checking if the regex matches
+  // for i := range commands.Commands {
+  //   fmt.Println(commands)
+  //
+  // }
+
 }
 
 
